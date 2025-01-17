1415cc922bfcf2e34fc590fda95d68cde8d5174c
RyanDusty <RyanDusty@users.noreply.github.com>
2025-01-18 03:32:50 +1100

    Added getlog to java,added hologram API, changed some plugin shit?

--- src/main/java/org/CustomFishingTraps.java
+++ src/main/java/org/CustomFishingTraps.java
@@ -1,17 +1,22 @@
 package org;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
 
 public final class CustomFishingTraps extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info("CustomFishingTraps has loaded!");
+        // We need to find the instance. Get the instance, if the instance isn't null, then we can run the getLogger that the plugin has been initialised.
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("CustomFishingTraps has been disabled!");
+
     }
 }
