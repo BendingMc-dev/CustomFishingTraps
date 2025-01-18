d7b591f1244f5f1e83f8a0fe969a8d9329734609
RyanDusty <RyanDusty@users.noreply.github.com>
2025-01-18 22:51:01 +1100

    Added IA and CF API, Added messages for when API's load

--- src/main/java/org/CustomFishingTraps.java
+++ src/main/java/org/CustomFishingTraps.java
@@ -1,22 +1,40 @@
 package org;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import dev.lone.itemsadder.api.ItemsAdder;
+import eu.decentsoftware.holograms.api.DecentHologramsAPI;
+import net.momirealms.customfishing.api.BukkitCustomFishingPlugin;
+import net.momirealms.customfishing.common.plugin.CustomFishingPlugin;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CustomFishingTraps extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        // Plugin start up logic
         getLogger().info("CustomFishingTraps has loaded!");
-        // We need to find the instance. Get the instance, if the instance isn't null, then we can run the getLogger that the plugin has been initialised.
-
+        // Decent Holograms API loaded, no idea why I couldn't use else???
+        if (DecentHologramsAPI.isRunning()) {
+            getLogger().info("Decent Holograms API has loaded for CustomFishingTraps!");
+        if (!DecentHologramsAPI.isRunning()) {
+                getLogger().info("Decent Holograms API hasn't loaded for CustomFishingTraps!");
+        // Items adder API Loaded, apparently areItemsLoaded is deprecated, but everything else I tried was broken.
+        if (ItemsAdder.areItemsLoaded())
+            getLogger().info("Itemsadder API is loaded for Custom Fishing Traps!");
+        else
+            getLogger().info("Itemsadder API isn't loaded for Custom Fishing Traps!"); }
+        // Custom Fishing API Loaded
+            BukkitCustomFishingPlugin customFishingApi = BukkitCustomFishingPlugin.getInstance();
+        if (customFishingApi == null)
+            getLogger().info("Custom Fishing api hasn't loaded!");
+        else
+            getLogger().info("Custom Fishing api has loaded!");
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
         getLogger().info("CustomFishingTraps has been disabled!");
-
     }
 }
